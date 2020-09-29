7b39489476ede508dbef239f52b6c902f525f153
LorisDonovan <a7skhy@gmail.com>
2020-09-29 21:37:29 +0545

    premake script added; torch libs added;

--- RL_Pokemon/src/main.cpp
+++ RL_Pokemon/src/main.cpp
@@ -1,6 +1,11 @@
 // we will probably include standard headers in a precompiled header
+#include "torch/torch.h"
+#include <iostream>
 
 int main(int argc, char* argv[])
 {
+	torch::Tensor input = torch::randn({ 1,2 });
+	std::cout << input << std::endl;
+	std::cin.get();
 	return 0;
 }
