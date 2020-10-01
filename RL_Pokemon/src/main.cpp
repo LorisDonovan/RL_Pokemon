0df2e26918cba19e4b5abf54523a4cd81b0674a0
LorisDonovan <a7skhy@gmail.com>
2020-10-01 13:41:17 +0545

    Replay Memory added

--- RL_Pokemon/src/main.cpp
+++ RL_Pokemon/src/main.cpp
@@ -1,9 +1,40 @@
 #include "pch.h"
+#include "dqn/ReplayMemory.h"
 
 int main(int argc, char* argv[])
 {
-	torch::Tensor input = torch::randn({ 1,2 });
-	std::cout << input << std::endl;
+	uint64_t capacity = 4;
+	uint32_t batchSize = 2;
+	uint32_t numStates = 1;
+	torch::Device device = torch::kCUDA;
+	ReplayMemory mem(capacity, batchSize, numStates, device);
+
+	torch::Tensor state		= torch::randn({ 1, numStates }, device);
+	torch::Tensor action	= torch::ones({ 1, 1 }, torch::kInt64).to(device); // has to be int64 for torch::gather() index
+	torch::Tensor nextState	= torch::randn({ 1, numStates }, device);
+	torch::Tensor reward	= torch::randn({ 1, 1 }, device);
+	torch::Tensor done		= torch::ones({ 1, 1 }, torch::kInt8).to(device);
+
+	Experience exp(state, action, nextState, reward, done);
+	
+	mem.Push(exp);
+	mem.Push(exp);
+	mem.Push(exp);
+	mem.Push(exp);
+	mem.Push(exp);
+	mem.Push(exp);
+
+	if (mem.CanProvideSample(batchSize))
+		exp = mem.Sample(batchSize);
+
+	std::cout << "Sampling..." << std::endl;
+	std::cout << "State: \n" << exp.State << std::endl;
+	std::cout << "Action: \n" << exp.Action << std::endl;
+	std::cout << "NextState: \n" << exp.NextState << std::endl;
+	std::cout << "Reward: \n" << exp.Reward << std::endl;
+	std::cout << "Done: \n" << exp.Done << std::endl;
+
+
 	std::cin.get();
 	return 0;
 }
